d40b927d936670843c180ec14cd7ec8942a78103
Bouke <BoukeWeel@outlook.com>
2019-11-18 12:30:55 +0100

    movetrashcan

---
+++ Project save the turtels/Assets/scripts/MoveTrashcan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrashcan : MonoBehaviour
+{
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+
+        offset = gameObject.transform.position -
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+    }
+}
---
+++ Project save the turtels/Assets/scripts/Trash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public Vector2 velocity;
+    private Rigidbody2D rig2d;
+    private void Start()
+    {
+        rig2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        velocity = new Vector2(5f, 5f);
+        rig2d.MovePosition(rig2d.position + velocity * Time.deltaTime);
+    }
+}
