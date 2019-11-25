fc457ca54359ae69e12fe9dc3725992145c95525
Quinten Reijns <529357@student.glu.nl>
2019-11-25 11:03:32 +0100

    Restart op goeie plek

---
+++ Project save the turtels/Assets/scripts/ResetButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetButton : MonoBehaviour
+{
+    public Button resetbuttonOne;
+    public Button resetbuttonTwo;
+    
+
+    private void Start()
+    {
+        resetbuttonTwo.gameObject.SetActive(true);
+    }
+
+    public void ResetButtonOne()
+    {
+        resetbuttonTwo.gameObject.SetActive(true);
+        resetbuttonOne.gameObject.SetActive(false);
+    } 
+
+}
