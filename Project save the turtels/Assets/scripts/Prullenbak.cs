d3119c3d3c08ea09a53cd3f156fd37cca0c7fd80
Bouke <BoukeWeel@outlook.com>
2019-12-14 11:31:34 +0100

    even iets testen

---
+++ Project save the turtels/Assets/scripts/Prullenbak.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Prullenbak : MonoBehaviour
+{
+    public int stufinprullenbak;
+    public int fullPrullebakint;
+
+    public int Prullenbaknummer;
+
+    public Text vullen_text;
+    bool isvol = false;
+
+
+    public void AddPrull()
+    {
+        stufinprullenbak++;
+    }
+    private void Update()
+    {
+       
+        vullen_text.text = "afval in prullenbak"+ Prullenbaknummer.ToString() + " " + stufinprullenbak.ToString();
+        if(isvol == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                AddPrull();
+            }
+            if (stufinprullenbak == fullPrullebakint)
+            {
+                Prullebakvol();
+            }
+        }
+        if(isvol == true)
+        {
+            print("o ja ");
+        }
+       
+    }
+    public void Prullebakvol()  
+    {
+        isvol = true;
+    }
+   
+
+}
