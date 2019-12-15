c5706c8ccac82ef6836e76c47b9672f5ec601007
Bouke <BoukeWeel@outlook.com>
2019-12-15 15:51:55 +0100

    prullen pakken legen
    nu kan je de prulllen bakken legen bij de vierkanten onder in(allen de prullenbak met de zelfde kleur als het vierkant kan geleegt worden)

--- Project save the turtels/Assets/scripts/Prullenbak.cs
+++ Project save the turtels/Assets/scripts/Prullenbak.cs
@@ -11,16 +11,29 @@ public class Prullenbak : MonoBehaviour
     public int Prullenbaknummer;
 
     public Text vullen_text;
+
     bool isvol = false;
+
+    public bool legen = false;
+
+    public float time = 0.5f;
 
 
     public void AddPrull()
     {
         stufinprullenbak++;
     }
-    private void Update()
+    private void FixedUpdate()
     {
-       
+        if (stufinprullenbak >= fullPrullebakint)
+        {
+            Prullebakvol();
+        }
+        if(stufinprullenbak < fullPrullebakint)
+        {
+            isvol = false;
+        }
+        
         vullen_text.text = "afval in prullenbak"+ Prullenbaknummer.ToString() + " " + stufinprullenbak.ToString();
         if(isvol == false)
         {
@@ -28,21 +41,72 @@ public class Prullenbak : MonoBehaviour
             {
                 AddPrull();
             }
-            if (stufinprullenbak == fullPrullebakint)
-            {
-                Prullebakvol();
-            }
+
         }
         if(isvol == true)
         {
             print("o ja ");
         }
+        if(legen == true)
+        {
+
+
+            
+            
+            if (stufinprullenbak > 0)
+            {
+                time -= Time.deltaTime;
+                if (time < 0)
+                {
+                    stufinprullenbak--;
+                    
+                    time = 0.5f;
+                }
+            }
+        }
+        
        
     }
+    
     public void Prullebakvol()  
     {
         isvol = true;
     }
-   
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(Prullenbaknummer == 1)
+        {
+            if (collision.CompareTag("btc1"))
+            {
+                legen = true;
+            }
+        }
+        if(Prullenbaknummer == 2)
+        {
+            if (collision.CompareTag("btc2"))
+            {
+                legen = true;
+            }
+        }
+        if(Prullenbaknummer == 3)
+        {
+            if (collision.CompareTag("btc3"))
+            {
+                legen = true;
+            }
+        }
+        if(Prullenbaknummer == 4)
+        {
+            if (collision.CompareTag("btc4"))
+            {
+                legen = true;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        legen = false;
+    }
+
 
 }
